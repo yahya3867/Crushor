d04a96d006d3b024ce2412e690996cfef83a6d4f
Yahya Masri <155339609+yahya3867@users.noreply.github.com>
2024-11-08 23:31:02 -0500

    Create JumpPad.cs

---
+++ JumpPad.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class JumpPad : MonoBehaviour
+{
+    [SerializeField] float bounce = 20f;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
+        }
+    }
+}
