cbc26852a24ef88822ef073c6f68f1974666fda3
Yahya Masri <155339609+yahya3867@users.noreply.github.com>
2024-11-08 23:18:15 -0500

    Create Teleporter.cs

---
+++ Teleporter.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+
+    [SerializeField] private Transform destination;
+
+    public Transform GetDestination()
+    {
+        return destination;
+    }
+
+}
